ef515adfa38c9e22f9b131589fc19a40910ca82b
Andre Celere <acelere@gmail.com>
2019-01-21 17:31:07 -0500

    change repo
    change repo old one did not have packages available

--- rk_install.r
+++ rk_install.r
@@ -1,3 +1,3 @@
-install.packages(c('rzmq','repr','IRkernel','IRdisplay'), repos = 'http://irkernel.github.io/', type = 'source')
+install.packages(c('rzmq','repr','IRkernel','IRdisplay'), repos = 'http://vps.fmvz.usp.br/CRAN/', type = 'source')
 IRkernel::installspec(user = FALSE)
 quit()
