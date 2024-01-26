8d1d1801b9d589846eb0f1a6cecf78a9caae47ab
Andre Celere <acelere@gmail.com>
2024-01-26 09:01:50 -0500

    update ESALQ URL form htto to https

--- rk_install.r
+++ rk_install.r
@@ -1,3 +1,3 @@
-install.packages(c('glue', 'cli', 'pillar', 'repr', 'rzmq','repr','IRkernel','IRdisplay'), repos = 'http://brieger.esalq.usp.br/CRAN/', type = 'source')
+install.packages(c('glue', 'cli', 'pillar', 'repr', 'rzmq','repr','IRkernel','IRdisplay'), repos = 'https://brieger.esalq.usp.br/CRAN/', type = 'source')
 IRkernel::installspec(user = FALSE)
 quit()
